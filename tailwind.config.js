e4dcdfeea4181fa2593033877c6ef17b8ffc72e7
BruninTI <bruno.araujo@viannasempre.com.br>
2022-06-03 18:32:39 -0300

    feat: add primary color on theme in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./srx/components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#01579B",
+      },
+    },
   },
   plugins: [],
 };
