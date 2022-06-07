339f01c05728770f55147f50ad569a0873b40afa
BruninTI <bruno.araujo@viannasempre.com.br>
2022-06-07 09:27:43 -0300

    feat: creating news list component

---
+++ src/components/molecules/news-list.tsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+// export interface NewListProps {
+//     news: NewsType[]
+// }
+// const NewList: React.FC<NewListProps> = ({news}) => {
+//     return (
+//         <>
+
+//         </>
+//     )
+// }
+
+// export default NewList
